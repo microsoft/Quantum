286c2744a038beff5bb0a729603d05031c8685e3
RolfHuisman <39134931+RolfHuisman@users.noreply.github.com>
2018-06-21 10:02:53 +0200

    Open quasm runner, to allow Q# programs to be run on the Quantum Experience (#59)
    * OpenQASM runner and IBM Quantum Experience integration
    
    * Making the runner, run in the same directory.
    
    * Testing Platforms
    
    * Unnessesary enter
    
    * Process Review Feedback
    Quasm => Qasm
    Annotated requirements.txt
    
    * Removing names

---
+++ Samples/Qiskit/IbmQx5.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Quantum.Samples.Qiskit
+{
+    /*
+     * Quick and dirty driver to enable the IbmQx5
+     */
+    class IbmQx5 : QiskitDriver
+    {
+        public IbmQx5(string key) : base(key)
+        {
+        }
+
+        public override int QBitCount => 16;
+
+        public override string Name => "ibmqx5";
+    }
+}
