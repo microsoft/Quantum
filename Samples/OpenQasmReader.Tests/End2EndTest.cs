ec2b37c7a6c8bd28daee0ea15346b3bd92a027d5
Rolf Huisman <rlrhuisman@outlook.com>
2018-09-10 17:20:23 +0200

    QFT's are generated by tooling. In EU this should result in copyright for person, but IANAL so removing to be sure. Teleport and rotations are based on paper => unclear so also remove.

--- Samples/OpenQasmReader.Tests/End2EndTest.cs
+++ Samples/OpenQasmReader.Tests/End2EndTest.cs
@@ -22,20 +22,7 @@ namespace Microsoft.Quantum.Samples.OpenQasmReader.Tests
         public void CNotConversionTest() => TestConversion("CNot.qs", $"{SOURCE_NAMESPACE}.CNot.qasm", $"{TARGET_NAMESPACE}.CNot.qs");
 
         [Fact]
-        public void FlipConversionTest() => TestConversion("Flip.qs", $"{SOURCE_NAMESPACE}.Flip.qasm", $"{TARGET_NAMESPACE}.Flip.qs");
-
-        [Fact]
-        public void TeleportConversionTest() => TestConversion("Teleport.qs", $"{SOURCE_NAMESPACE}.Teleport.qasm", $"{TARGET_NAMESPACE}.Teleport.qs");
-
-        [Fact]
         public void GatesConversionTest() => TestConversion("Gates.qs", $"{SOURCE_NAMESPACE}.Gates.qasm", $"{TARGET_NAMESPACE}.Gates.qs");
-
-        [Fact]
-        public void RotationsConversionTest() => TestConversion("Rotations.qs", $"{SOURCE_NAMESPACE}.Rotations.qasm", $"{TARGET_NAMESPACE}.Rotations.qs");
-        [Fact]
-        public void Qft30ConversionTest() => TestConversion("Qft_n30.qs", $"{SOURCE_NAMESPACE}.qft_n30.qasm", $"{TARGET_NAMESPACE}.Qft30.qs");
-        [Fact]
-        public void Qft40ConversionTest() => TestConversion("Qft_n40.qs", $"{SOURCE_NAMESPACE}.qft_n40.qasm", $"{TARGET_NAMESPACE}.Qft40.qs");
 
         private static void TestConversion(string name, string inputResourceName, string expectedResourceName)
         {
--- Samples/OpenQasmReader.Tests/TestFunctions/Gates.qasm
+++ Samples/OpenQasmReader.Tests/TestFunctions/Gates.qasm
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 OPENQASM 2.0;
-
 gate majority a,b,c 
 { 
   cx c,b; 
