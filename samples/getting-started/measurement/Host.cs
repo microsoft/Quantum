a64dae5e94850fed445b2454bd0368a58fc133de
Christopher Granade <cgranade@cgranade.com>
2019-10-01 15:12:27 -0700

    More work towards extracting getting started samples.

---
+++ samples/getting-started/measurement/Host.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+
+using Microsoft.Quantum.Simulation.Simulators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Quantum.Samples.Measurement
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            using (var qsim = new QuantumSimulator())
+            {
+                RunQuantumMain.Run(qsim).Wait();
+            }
+
+        }
+    }
+}
