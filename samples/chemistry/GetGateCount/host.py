57a9d3461db7cbf0de52adcdb691b4618b4a6ec3
JoePagani <josephepagani@gmail.com>
2019-10-31 17:59:48 -0700

    removed unfinished chemistry samples

--- samples/chemistry/GetGateCount/host.py
+++
@@ -1,7 +0,0 @@
-import argparse
-import qsharp
-from pprint import pprint
-from qsharp.chemistry import load_broombridge, load_fermion_hamiltonian, IndexConvention, encode
-from Microsoft.Quantum.Chemistry.Samples import TrotterEstimateEnergy, OptimizedTrotterEstimateEnergy, QubitizationEstimateEnergy
-qsharp.packages.add("microsoft.quantum.research")
-qsharp.reload()
--- samples/chemistry/SimulateHubbardHamiltonian/host.py
+++
@@ -1,4 +0,0 @@
-import argparse
-import numpy as np
-from numpy import linalg as LA
-from qsharp.chemistry import load_broombridge, load_fermion_hamiltonian, IndexConvention
