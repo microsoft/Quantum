9a13a0652f445cdc8fc31301109465562cddb955
Mathias Soeken <mathias.soeken@epfl.ch>
2020-05-04 22:05:19 +0200

    Rename files and add copyright headers.

--- samples/runtime/reversible-simulator/Operation.qs
+++ samples/runtime/reversible-simulator/Operation.qs
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 namespace Microsoft.Quantum.Samples {
     open Microsoft.Quantum.Canon;
     open Microsoft.Quantum.Intrinsic;
--- samples/runtime/reversible-simulator/Simulator.cs
+++ samples/runtime/reversible-simulator/Simulator.cs
@@ -1,5 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Quantum.Simulation.QuantumProcessor;
 using Microsoft.Quantum.Simulation.Common;
 using Microsoft.Quantum.Simulation.Core;
