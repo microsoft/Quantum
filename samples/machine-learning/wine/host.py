66f49e474b26103776ba8f71e28a45a1b675e8b9
Ryan Shaffer <ryansha@microsoft.com>
2020-07-02 02:56:50 +0000

    Build 0.12.2007.124.

--- samples/machine-learning/wine/host.py
+++ samples/machine-learning/wine/host.py
@@ -2,7 +2,7 @@
 # Licensed under the MIT License.
 
 import qsharp
-qsharp.packages.add("Microsoft.Quantum.MachineLearning::0.11.2004.2825")
+qsharp.packages.add("Microsoft.Quantum.MachineLearning::0.12.20070124")
 qsharp.reload()
 
 from Microsoft.Quantum.Samples import TrainWineModel, ValidateWineModel
