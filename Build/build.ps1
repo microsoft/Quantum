3e226144cc5a1ec6946843d92b1aef7d8be2bfc0
Stefan J. Wernli <swernli@microsoft.com>
2021-01-30 23:34:27 -0800

    Add clean-up logic to build script to save space

--- Build/build.ps1
+++ Build/build.ps1
@@ -23,6 +23,12 @@ function Build-One {
         Write-Host "##vso[task.logissue type=error;]Failed to build $project."
         $script:all_ok = $False
     }
+
+    if ($env:FORCE_CLEANUP -eq "true") {
+        # Force cleanup of generated bin and obj folders for this project.
+        Write-Host "##[info]Cleaning up bin/obj from $(Split-Path $project -Parent)..."
+        Get-ChildItem -Path (Split-Path $project -Parent) -Recurse | Where-Object { ($_.name -eq "bin" -or $_.name -eq "obj") -and $_.attributes -eq "Directory" } | Remove-Item -recurse -force
+    }
 }
 
 Get-ChildItem (Join-Path $PSScriptRoot '..') -Recurse -Include '*.sln' `
