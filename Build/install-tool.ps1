c4c52c750ceb4f91278943c2b32c8f0ab951c79c
Scott Carda <sccarda@microsoft.com>
2021-06-24 18:54:10 -0700

    More verbose install script

--- Build/install-tool.ps1
+++ Build/install-tool.ps1
@@ -1,9 +1,16 @@
 ## This script is temporary
 
+Write-Host "##[info]Beginning qir-cli tool installation script";
+
 if (-not (Get-Command qir-cli -ErrorAction SilentlyContinue)) {
     $version = ($Env:NUGET_VERSION -split "-")[0] + "-alpha"
+
+    Write-Host "##[info]Going to install version $version of the command line tool.";
+
     dotnet tool install Microsoft.Quantum.Qir.CommandLineTool --version $version -g
-    Write-Host "##[info]The qir-cli command has been installed.";
+    Write-Host "##[info]The qir-cli command has been installed with version $version";
 } else {
     Write-Host "##[info]The qir-cli command is already installed.";
 }
+
+Write-Host "##[info]Ending qir-cli tool installation script";
