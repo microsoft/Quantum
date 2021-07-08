efe5bca04a7caeaa556d6fdf9e39af8e95a12e6f
Scott Carda <55811729+ScottCarda-MS@users.noreply.github.com>
2021-07-08 15:42:33 -0700

    QIR Validation Script - Quantum (#519)

--- Build/validate-qir.ps1
+++ Build/validate-qir.ps1
@@ -1,6 +1,12 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+# Important! The validation logic requires certain tools to work.
+# This script will install the necessary tools if they are not already preset!
+# This includes the Microsoft.Quantum.Qir.CommandLineTool dotnet tool and
+# Clang OR LLVM. If on Windows and clang is not available, it will try to
+# install LLVM with Chocolatey.
+
 $ErrorActionPreference = 'Stop'
 
 & "$PSScriptRoot/set-env.ps1"
@@ -10,7 +16,7 @@ function Build-One {
     param(
         $Project,
         $Arguments
-    );
+    )
 
     $projectDirectory = $(Split-Path $Project -Parent)
     $projectName = $(Split-Path $Project -LeafBase)
@@ -30,19 +36,33 @@ function Build-One {
         Write-Host "##vso[task.logissue type=error;]Failed to build $Project."
         $script:allOk = $False
     } else {
+        Write-Host "##[info]Compiling $projectName to executable..."
         qir-cli `
             --dll (Join-Path bin $Env:BUILD_CONFIGURATION netcoreapp3.1 "${projectName}.dll") `
             --exe qir
-        if  ($LastExitCode -ne 0) {
-            Write-Host "##vso[task.logissue type=error;]Failed to build $Project."
+        if  ($LastExitCode -ne 0 -or (Get-ChildItem (Join-Path qir *__Interop.exe) -ErrorAction SilentlyContinue).Count -eq 0) {
+            Write-Host "##vso[task.logissue type=error;]Failed to compile $projectName to executable."
             $script:allOk = $False
         } else {
+            Write-Host "##[info]Updating environment path variables."
+            $qirPath = Join-Path $projectDirectory qir
+            $old_DYLD_LIBRARY_PATH = $env:DYLD_LIBRARY_PATH
+            $old_LD_LIBRARY_PATH = $env:LD_LIBRARY_PATH
+            $env:DYLD_LIBRARY_PATH += ":${qirPath}:"
+            $env:LD_LIBRARY_PATH += ":${qirPath}:"
+            
+            Write-Host "##[info]Running $projectName..."
             Get-ChildItem (Join-Path qir *__Interop.exe) `
                 | ForEach-Object { & $_ @Arguments}
             if  ($LastExitCode -ne 0) {
-                Write-Host "##vso[task.logissue type=error;]$Project encountered an error or failed during execution."
+                Write-Host "##vso[task.logissue type=error;]$projectName encountered an error or failed during execution."
                 $script:allOk = $False
+            } else {
+                Write-Host "##[info]QIR validation against $projectName was successful."
             }
+
+            $env:DYLD_LIBRARY_PATH = $old_DYLD_LIBRARY_PATH
+            $env:LD_LIBRARY_PATH = $old_LD_LIBRARY_PATH
         }
     }
 
@@ -57,23 +77,24 @@ function Build-One {
 
 # The commented out lines are sample projects that are not yet compatible for QIR generation/execution. 
 # 'not compatible' means that the structure of the sample is not compatible for QIR generation.
-# 'needs argument(s)' means that the sample can generated QIR, but running the exe requires command line arguments.
+# 'relies on hardware' means that the sample targets a hardware architecture that is not yet supported for QIR.
+# 'memory leak' means that the sample stalls when running and uses way too many system resources. Tracked with https://github.com/microsoft/qsharp-runtime/issues/757.
 $qirProjects = @(
     # not compatible #@{ Path = (Join-Path $PSScriptRoot .. samples algorithms chsh-game CHSHGame.csproj); Args = @() },
     # not compatible #@{ Path = (Join-Path $PSScriptRoot .. samples algorithms database-search DatabaseSearchSample.csproj); Args = @() },
     # not compatible #@{ Path = (Join-Path $PSScriptRoot .. samples algorithms integer-factorization IntegerFactorization.csproj); Args = @() },
     @{ Path = (Join-Path $PSScriptRoot .. samples algorithms oracle-synthesis OracleSynthesis.csproj); Args = @() },
     @{ Path = (Join-Path $PSScriptRoot .. samples algorithms order-finding OrderFinding.csproj); Args = @("--index", "1") },
-    @{ Path = (Join-Path $PSScriptRoot .. samples algorithms repeat-until-success RepeatUntilSuccess.csproj); Args = @("--gate", "simple", "--input-value", "true", "--input-basis", "PauliX", "--limit", "4", "--num-runs", "2") },
+    @{ Path = (Join-Path $PSScriptRoot .. samples algorithms repeat-until-success RepeatUntilSuccess.csproj); Args = @("--gate", "simple", "--inputValue", "true", "--inputBasis", "PauliX", "--limit", "4", "--numRuns", "2") },
     @{ Path = (Join-Path $PSScriptRoot .. samples algorithms reversible-logic-synthesis ReversibleLogicSynthesis.csproj); Args = @() },
     @{ Path = (Join-Path $PSScriptRoot .. samples algorithms simple-grover SimpleGroverSample.csproj); Args = @("--nQubits", "8") },
     # not compatible #@{ Path = (Join-Path $PSScriptRoot .. samples algorithms sudoku-grover SudokuGroverSample.csproj); Args = @() },
 
-    # needs argument(s) #@{ Path = (Join-Path $PSScriptRoot .. samples azure-quantum grover Grover.csproj); Args = @() },
-    # needs argument(s) #@{ Path = (Join-Path $PSScriptRoot .. samples azure-quantum hidden-shift HiddenShift.csproj); Args = @() },
-    # needs argument(s) #@{ Path = (Join-Path $PSScriptRoot .. samples azure-quantum ising-model IsingModel.csproj); Args = @() },
-    # needs argument(s) #@{ Path = (Join-Path $PSScriptRoot .. samples azure-quantum parallel-qrng ParallelQrng.csproj); Args = @() },
-    # needs argument(s) #@{ Path = (Join-Path $PSScriptRoot .. samples azure-quantum teleport Teleport.csproj); Args = @() },
+    @{ Path = (Join-Path $PSScriptRoot .. samples azure-quantum grover Grover.csproj); Args = @("--nQubits", "3", "--idxMarked", "6") },
+    @{ Path = (Join-Path $PSScriptRoot .. samples azure-quantum hidden-shift HiddenShift.csproj); Args = @("--patternInt", "6", "--registerSize", "4") },
+    @{ Path = (Join-Path $PSScriptRoot .. samples azure-quantum ising-model IsingModel.csproj); Args = @("--nSites", "5", "--time", "5.0", "--dt", "0.1") },
+    @{ Path = (Join-Path $PSScriptRoot .. samples azure-quantum parallel-qrng ParallelQrng.csproj); Args = @("--nQubits", "3") },
+    # relies on hardware #@{ Path = (Join-Path $PSScriptRoot .. samples azure-quantum teleport Teleport.csproj); Args = @("--prepBasis", "PauliX", "--measBasis", "PauliX") },
 
     @{ Path = (Join-Path $PSScriptRoot .. samples characterization phase-estimation PhaseEstimationSample.csproj); Args = @() },
 
@@ -89,35 +110,71 @@ $qirProjects = @(
 
     @{ Path = (Join-Path $PSScriptRoot .. samples getting-started measurement Measurement.csproj); Args = @() },
     @{ Path = (Join-Path $PSScriptRoot .. samples getting-started qrng Qrng.csproj); Args = @() },
-    # needs argument(s) #@{ Path = (Join-Path $PSScriptRoot .. samples getting-started simple-algorithms SimpleAlgorithms.csproj); Args = @() },
-    @{ Path = (Join-Path $PSScriptRoot .. samples getting-started teleportation TeleportationSample.csproj); Args = @() }
+    @{ Path = (Join-Path $PSScriptRoot .. samples getting-started simple-algorithms SimpleAlgorithms.csproj); Args = @("--nQubits", "4") }
+    @{ Path = (Join-Path $PSScriptRoot .. samples getting-started teleportation TeleportationSample.csproj); Args = @() },
 
     # not compatible #@{ Path = (Join-Path $PSScriptRoot .. samples machine-learning half-moons HalfMoons.csproj); Args = @() },
     # not compatible #@{ Path = (Join-Path $PSScriptRoot .. samples machine-learning parallel-half-moons ParallelHalfMoons.csproj); Args = @() },
     # not compatible #@{ Path = (Join-Path $PSScriptRoot .. samples machine-learning wine Wine.csproj); Args = @() },
 
     @{ Path = (Join-Path $PSScriptRoot .. samples numerics CustomModAdd CustomModAdd.csproj); Args = @() },
-    @{ Path = (Join-Path $PSScriptRoot .. samples numerics EvaluatingFunctions EvaluatingFunctions.csproj); Args = @() },
-    @{ Path = (Join-Path $PSScriptRoot .. samples numerics ResourceCounting ResourceCounting.csproj); Args = @() },
+    # memory leak #@{ Path = (Join-Path $PSScriptRoot .. samples numerics EvaluatingFunctions EvaluatingFunctions.csproj); Args = @() },
+    # memory leak #@{ Path = (Join-Path $PSScriptRoot .. samples numerics ResourceCounting ResourceCounting.csproj); Args = @() },
 
     # not compatible #@{ Path = (Join-Path $PSScriptRoot .. samples simulation h2 command-line H2SimulationSampleCmdLine.csproj); Args = @() },
-    @{ Path = (Join-Path $PSScriptRoot .. samples simulation hubbard HubbardSimulationSample.csproj); Args = @() }
+    @{ Path = (Join-Path $PSScriptRoot .. samples simulation hubbard HubbardSimulationSample.csproj); Args = @() },
     # not compatible #@{ Path = (Join-Path $PSScriptRoot .. samples simulation ising adiabatic AdiabaticIsingSample.csproj); Args = @() },
     # not compatible #@{ Path = (Join-Path $PSScriptRoot .. samples simulation ising generators IsingGeneratorsSample.csproj); Args = @() },
     # not compatible #@{ Path = (Join-Path $PSScriptRoot .. samples simulation ising phase-estimation IsingPhaseEstimationSample.csproj); Args = @() },
     # not compatible #@{ Path = (Join-Path $PSScriptRoot .. samples simulation ising simple SimpleIsingSample.csproj); Args = @() },
     # not compatible #@{ Path = (Join-Path $PSScriptRoot .. samples simulation ising trotter-evolution IsingTrotterSample.csproj); Args = @() },
-    # needs argument(s) #@{ Path = (Join-Path $PSScriptRoot .. samples simulation qaoa QAOA.csproj); Args = @() }
+    @{ Path = (Join-Path $PSScriptRoot .. samples simulation qaoa QAOA.csproj); Args = @("--numTrials", "20") }
 )
 
+Write-Host "##[info]Beginning Microsoft.Quantum.Qir.CommandLineTool installation."
 if (-not (Get-Command qir-cli -ErrorAction SilentlyContinue)) {
-    Write-Host "##[error]The qir-cli command is missing; you can install it by running `dotnet tool install Microsoft.Quantum.Qir.CommandLineTool -g`.";
-    $script:allOk = $False
+    $version = ($Env:NUGET_VERSION -split "-")[0] + "-alpha"
+    Write-Host "##[info]Going to install Microsoft.Quantum.Qir.CommandLineTool.$version."
+    dotnet tool install Microsoft.Quantum.Qir.CommandLineTool --version $version -g
+    if  ($LastExitCode -ne 0) {
+        Write-Host "##[error]Failed to install Microsoft.Quantum.Qir.CommandLineTool.$version."
+        $script:allOk = $False
+    } else {
+        Write-Host "##[info]The Microsoft.Quantum.Qir.CommandLineTool.$version has been installed."
+    }
 } else {
-    $qirProjects `
-        | ForEach-Object { Build-One $_.Path $_.Args }
+    Write-Host "##[info]The Microsoft.Quantum.Qir.CommandLineTool is already installed."
 }
 
-if (-not $allOk) {
-    throw "At least one sample failed build. Check the logs."
+if ($allOk) {
+    $updatePath = ($IsWindows -or $PSVersionTable.PSEdition -eq "Desktop") -and !(Get-Command clang -ErrorAction SilentlyContinue)
+    if ($updatePath) {
+        $old_PATH = $env:PATH
+        if ((choco find --idonly -l llvm) -notcontains "llvm") {
+            Write-Host "##[info]Installing LLVM with Chocolatey."
+            choco install llvm --version=11.1.0
+        }
+
+        # Check to make sure the installation was successful.
+        if ((choco find --idonly -l llvm) -contains "llvm") {
+            Write-Host "##[info]LLVM was installed by Chocolatey, so adding the install location to PATH."
+            $env:PATH += ";$($env:SystemDrive)\Program Files\LLVM\bin"
+        } else {
+            throw "Unable to find an installation of LLVM."
+        }
+    }
+
+    Write-Host "##[info]Running Validation of QIR against Samples."
+    $qirProjects `
+        | ForEach-Object { Build-One $_.Path $_.Args }
+    if ($updatePath) {
+        $env:PATH = $old_PATH;
+    }
+    if (-not $allOk) {
+        throw "At least one sample failed build. Check the logs."
+    } else {
+        Write-Host "##[info]Validation of QIR against Samples was successful."
+    }
+} else {
+    throw "The Microsoft.Quantum.Qir.CommandLineTool did not install successfully."
 }
