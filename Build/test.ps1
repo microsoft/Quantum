5c2c0170560a6b1b041470e21c139dff7cbedf8a
Sarah Marshall <33814365+samarsha@users.noreply.github.com>
2021-08-19 15:20:31 -0700

    Convert algorithm samples to standalone executables to enable QIR testing (#535)
    * Convert CHSH Game sample to standalone executable
    
    * Convert Database Search sample to standalone executable
    
    * Use internal
    
    * Add copyrights
    
    * Use relative links
    
    * Remove App.config
    
    * Apply suggestions from code review
    
    Co-authored-by: Chris Granade <chgranad@microsoft.com>
    
    * Fix typo in comment
    
    * Add comments to CHSH game
    
    * Use multiple entry points for Database Search
    
    * Use one code block per example
    
    * Improve wording
    
    Co-authored-by: Chris Granade <chgranad@microsoft.com>

--- Build/test.ps1
+++ Build/test.ps1
@@ -78,6 +78,12 @@ Test-One '../samples/diagnostics/unit-testing'
 # of arguments, so that we can test samples in different configurations
 # and against different simulators.
 $projectArgs = @{
+        "../samples/algorithms/database-search/DatabaseSearchSample.csproj" = @(,
+            @("simulate", "Microsoft.Quantum.Samples.DatabaseSearch.RunRandomSearch"),
+            @("simulate", "Microsoft.Quantum.Samples.DatabaseSearch.RunQuantumSearch"),
+            @("simulate", "Microsoft.Quantum.Samples.DatabaseSearch.RunMultipleQuantumSearch")
+        );
+
         "../samples/simulation/qaoa/QAOA.csproj" = @(,
             @("--num-trials", "10")
         );
